82ebb9f304a8932ee50de7e30c21e9a27bc53b1e
bitluffy <yashsingh.is22@rvce.edu.in>
2025-05-05 23:50:59 +0530

    first commit

---
+++ learn_sphere/backend/server.js
@@ -0,0 +1,8 @@
+const PORT = 5000;
+import express from "express";
+import cors from "cors";
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
